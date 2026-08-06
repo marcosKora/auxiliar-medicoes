cb9ab3fb884c8c66c81a3a8db485fefce6d4bf70
marcosKora <marcos.oliveira@korasaude.com.br>
2026-08-06 15:17:47 -0300

    Update auxMedWeb.py

--- auxMedWeb.py
+++ auxMedWeb.py
@@ -365,7 +365,7 @@ def executar_automacao(ids_processar, nome_perfil=None):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--window-size=1920,1080")
     
     if nome_perfil:
